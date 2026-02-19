4ae37b676a98575fbf3cc0cbb1f534b7f2ddd4f3
asak-coder <web.akengg@gmail.com>
2026-02-19 12:56:42 +0530

    Fix leadroute

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -38,6 +38,23 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+// UPDATE LEAD STATUS
+router.patch("/:id", async (req, res) => {
+  try {
+    const { status } = req.body;
+
+    const updatedLead = await Lead.findByIdAndUpdate(
+      req.params.id,
+      { status },
+      { new: true }
+    );
+
+    res.status(200).json(updatedLead);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 
 module.exports = router;
